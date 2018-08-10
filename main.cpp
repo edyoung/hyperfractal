d983dd96ac756df456a07fb286bdf409ede03e83
edwin young <edwin@bathysphere.org>
2018-08-10 15:50:56 -0700

    remove refs to scanner and llvm while doing cuda experiment

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
 
-#include "Scanner.h"
 
-#include "llvm/IR/Verifier.h"
-#include "llvm/IR/IRBuilder.h"
-
-using namespace llvm;
-
-static Module *TheModule;
-static IRBuilder<> Builder(getGlobalContext());
 
 int main()
 {
     std::cout << "hello\n";
 
-    Scanner scanner;
-    while(scanner.lex())
-    {
-	std::cout << scanner.matched() << std::endl;
-    }
     
-    Value *c1 = ConstantFP::get(getGlobalContext(), APFloat(2.0));
-    Value *c2 = ConstantFP::get(getGlobalContext(), APFloat(2.0));
-    Value *sum = Builder.CreateFAdd(c1,c2, "addtmp");
-
 }
